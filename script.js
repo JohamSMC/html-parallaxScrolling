c4e40cdac56fc87b0c88b4dcf350017a3f8ddbec
Joham Medina <joham.nba@gmail.com>
2020-07-03 02:33:52 -0500

    step Four

---
+++ script.js
@@ -0,0 +1,12 @@
+let bg = document.getElementById("bg")
+let moon = document.getElementById("moon")
+let mountain = document.getElementById("mountain")
+let road = document.getElementById("road")
+let title = document.getElementById("title")
+
+window.addEventListener("scroll", function(){
+    let value = window.scrollY;
+
+    bg.style.top = value * 0.5 + "px";
+    moon.style.left = -value * 0.5 + "px";
+})
