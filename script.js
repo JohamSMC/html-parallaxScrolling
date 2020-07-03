0f71fd91f2a22457932352b30c19e4bb67718aa1
Joham Medina <joham.nba@gmail.com>
2020-07-03 02:38:26 -0500

    step Five

--- script.js
+++ script.js
@@ -8,5 +8,8 @@ window.addEventListener("scroll", function(){
     let value = window.scrollY;
 
     bg.style.top = value * 0.5 + "px";
-    moon.style.left = -value * 0.5 + "px";
+    moon.style.left = -value * 0.8 + "px";
+    mountain.style.top = -value * 0.15 + "px";
+    road.style.top = value * 0.15 + "px";
+    title.style.top = value * 0.8 + "px";
 })
